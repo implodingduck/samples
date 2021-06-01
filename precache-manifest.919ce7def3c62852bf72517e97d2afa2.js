3ddf4c9c067029147bdacd3a2bd04e0dc8192482
implodingduck <zsek320@gmail.com>
2021-06-01 12:06:18 -0500

    Updates

---
+++ precache-manifest.919ce7def3c62852bf72517e97d2afa2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4345a0aa92a82f13c8f4c44888a93aa",
+    "url": "/samples/index.html"
+  },
+  {
+    "revision": "1a5292cde517e5159c8a",
+    "url": "/samples/static/css/2.a5f06b7f.chunk.css"
+  },
+  {
+    "revision": "208a49ac211a5ef871aa",
+    "url": "/samples/static/css/main.800e34da.chunk.css"
+  },
+  {
+    "revision": "1a5292cde517e5159c8a",
+    "url": "/samples/static/js/2.c5b79816.chunk.js"
+  },
+  {
+    "revision": "0d08bb566150086a6317785a01bb3007",
+    "url": "/samples/static/js/2.c5b79816.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "208a49ac211a5ef871aa",
+    "url": "/samples/static/js/main.a7848f1f.chunk.js"
+  },
+  {
+    "revision": "033ee2481372d0631f07",
+    "url": "/samples/static/js/runtime-main.300a14df.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/slaestimatorv2/precache-manifest.0d9a65637336b043fc34cec5fdfa03a2.js"
+  "/samples/precache-manifest.919ce7def3c62852bf72517e97d2afa2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/slaestimatorv2/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/samples/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
