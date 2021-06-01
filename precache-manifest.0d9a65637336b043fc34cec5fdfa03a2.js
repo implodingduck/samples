a7016554fd6f5d1ec80f4440b516db63cacc05b6
implodingduck <zsek320@gmail.com>
2021-06-01 12:04:09 -0500

    Updates

---
+++ precache-manifest.0d9a65637336b043fc34cec5fdfa03a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fd698896e6d181f0d443112e781a89f",
+    "url": "/slaestimatorv2/index.html"
+  },
+  {
+    "revision": "1a5292cde517e5159c8a",
+    "url": "/slaestimatorv2/static/css/2.a5f06b7f.chunk.css"
+  },
+  {
+    "revision": "4c347a61e7fbca9a3515",
+    "url": "/slaestimatorv2/static/css/main.800e34da.chunk.css"
+  },
+  {
+    "revision": "1a5292cde517e5159c8a",
+    "url": "/slaestimatorv2/static/js/2.c5b79816.chunk.js"
+  },
+  {
+    "revision": "0d08bb566150086a6317785a01bb3007",
+    "url": "/slaestimatorv2/static/js/2.c5b79816.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c347a61e7fbca9a3515",
+    "url": "/slaestimatorv2/static/js/main.6ee97f15.chunk.js"
+  },
+  {
+    "revision": "37596837c5f87506807a",
+    "url": "/slaestimatorv2/static/js/runtime-main.6ef8b400.js"
+  }
+]);
